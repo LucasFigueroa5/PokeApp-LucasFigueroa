dcadfcaa29fdf52b832978efbaed3d71a100e243
LucasFigueroa5 <lucasfigueroab@gmail.com>
2023-07-14 12:50:41 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+require('dotenv').config()
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, async () => {
+  server.listen(process.env.PORT, async () => {
     // const allPokemons = Pokemons.findAll
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log('%s listening', process.env.PORT ); // eslint-disable-line no-console
   });
 });
